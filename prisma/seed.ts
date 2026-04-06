212b0cb86b6a425a7a1ee6343f628be8fe68c7bd
Sarasaidin <sara@ever-technologies.com>
2026-04-06 09:24:53 +0800

    fix: added bcrypt import and updated build script

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,6 @@
 import bcrypt from "bcrypt";
 import { PrismaClient } from "@prisma/client";
-// import bcrypt from "bcryptjs";
+import bcrypt from "bcryptjs";
 
 const prisma = new PrismaClient();
 
